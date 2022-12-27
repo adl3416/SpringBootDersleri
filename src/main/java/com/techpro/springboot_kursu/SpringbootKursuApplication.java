8fb39e77ff0b8431514ecca3beb788763d87fbb1
adl <makara3416@gmail.com>
2022-12-27 01:01:01 +0100

    son

--- src/main/java/com/techpro/springboot_kursu/SpringbootKursuApplication.java
+++ src/main/java/com/techpro/springboot_kursu/SpringbootKursuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKursuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootKursuApplication.class, args); //acik notlarda a
+		SpringApplication.run(SpringbootKursuApplication.class, args);//ekledi
 	}
 
 }
