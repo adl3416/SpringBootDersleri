3de7dfe5e3b8f509e2c23276ec3e69895944f831
adl <makara3416@gmail.com>
2023-01-01 19:37:08 +0100

    son

--- src/main/java/springboot_kurs_controller_service_repository_form_based_authentication/SpringbootKursuApplication.java
+++ src/main/java/springboot_kurs_controller_service_repository_form_based_authentication/SpringbootKursuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKursuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootKursuApplication.class, args); // pom  yenilendi
+		SpringApplication.run(SpringbootKursuApplication.class, args); // enson
 	}
 
 }
