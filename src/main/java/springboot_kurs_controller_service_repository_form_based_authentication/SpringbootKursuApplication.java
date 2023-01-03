e2e902582f9fd55a3686d48589e42afcd3824c95
adl <makara3416@gmail.com>
2023-01-03 19:34:42 +0100

    pom

--- src/main/java/springboot_kurs_controller_service_repository_form_based_authentication/SpringbootKursuApplication.java
+++ src/main/java/springboot_kurs_controller_service_repository_form_based_authentication/SpringbootKursuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKursuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootKursuApplication.class, args);
+		SpringApplication.run(SpringbootKursuApplication.class, args); // pom  yenilendi
 	}
 
 }
